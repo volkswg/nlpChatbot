2e1061014cae5a4a2d222f2d9e4aec20584a52ab
PonGz <psupanatw@gmail.com>
2018-05-11 17:59:09 +0700

    fixing bug

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ import random
 def main():
 
 	no_list = ['no','not yet','not now','next time','no,']
-	hello_list = ['Hi!','halo','hello','bello','yo']
+	hello_list = ['hi','halo','hello','bello','yo']
 	item_list = ['t-shirt','pants']
 	agree_list = ['yes','sure','of course']
 	shipping_list = ['kerry','ems','dhl']
@@ -21,13 +21,38 @@ def main():
 			if i.lower() in hello_list :
 				helloIndex = random.randint(1,len(hello_list)-1)
 				print('Shop: ' + hello_list[helloIndex] + ', please say what you need.')
-			elif i.lower() == 'buy' or i.lower() == 'what':
+			elif i.lower() == 'buy' or i.lower() == 'what' or i.lower() == 'order' or i.lower() == 'want':
 				for i in proc:
-					if i.lower() == 'something' or i.lower() == 'product':
-						print("Now we have : ")
-						print("ID	 Product	Size 	 Color	 	Quantity")
-						print("001	 T-shirt 	S,M,L    Black	 	10")
-						print("002	 Pants 		S,M,L   red,blue 	10")
+					print("Now we have : ")
+					print("ID	 Product	Size 	 Color	 	Quantity")
+					print("001	 T-shirt 	S,M,L    Black	 	10")
+					print("002	 Pants 		S,M,L   red,blue 	10")
+					print("Shop: Do you like to order now?")
+					ans = input('Me: ')
+					ans_proc = ans.split(' ')
+					for j in ans_proc:
+						if j in agree_list:
+							print("Shop:  Order me")
+							ans2 = input('Me: ')
+							ans_proc2 = ans.split(' ')
+							for k in ans_proc2 :
+								print(k)
+								if k in item_list:
+									print("You order for " + k + ", What shipping method do you prefer?")
+									ans_shippping = input('Me: ')
+									shipping_proc = ans_shippping.split(' ')
+									shipping_check = 1
+									print("eiei")
+									while shipping_check == 1:
+										for l in shipping_proc:
+											if l in shipping_list:
+												print('I will send you in 2 days. Thanks for coming.')
+												shipping_check = 0
+											else :
+												print('We only have 3 shipping method. EMS, KERRY and DHL ')
+						
+
+
 			elif i.lower() == 'suggest':
 				x = random.randint(0,len(item_list)-1)
 				print("Shop: " + item_list[x] + ' is suggested for you.')
@@ -74,8 +99,23 @@ def test1():
 	print ("hah")
 	print ("hu")
 	print ("23")
-def test2():
-	print ("eiei2")
+def soundex_input(name):
+	name = name.upper()
+	soundex = ""
+	soundex += name[0]
+	dictionary = {"BFPV": "1", "CGJKQSXZ":"2", "DT":"3", "L":"4", "MN":"5", "R":"6", "AEIOUHWY":"."}
+
+	for char in name[1:]:
+		for key in dictionary.keys():
+			if char in key:
+				code = dictionary[key]
+				if code != soundex[-1]:
+					soundex += code
+	soundex = soundex.replace(".", "")
+	soundex = soundex[:4].ljust(4, "0")
+	return soundex
+
+
 		
 if __name__ == '__main__':
 	try:
