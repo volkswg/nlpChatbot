94818ec837347b8d54dd8d431d8da090e876451c
volkswg <volkswg@hotmail.com>
2018-05-11 14:46:12 +0700

    set Default word
    algorithim to count  the not understand word

--- chatbot.py
+++ chatbot.py
@@ -1,29 +1,32 @@
 import random
 
 no_list = ['no','not yet','not now','next time','no,']
+hello_list = ['Hi!','halo','hello','bello','yo']
 item_list = ['t-shirt','pants']
 agree_list = ['yes','sure','of course']
 
 endConversation = False
+noUnderstandCount = 0
 print("Shop: Hi, I'm a bot. Call me SellingBot.")
 print("Shop: How can I help you.")
 
 while endConversation == False:
+	noUnderstandCount = 0
 	text = input('Me: ') #What products are you have?
 	proc = text.split(' ')
 	for i in proc:
 		if i.lower() in hello_list :
 			
 			helloIndex = random.randint(1,len(hello_list)-1)
-			print('Shop: ' + hello_list[helloIndex])
-		if i.lower() == 'buy':
+			print('Shop: ' + hello_list[helloIndex] + ', please say what you need.')
+		elif i.lower() == 'buy':
 			for i in proc:
 				if i.lower() == 'something' or i.lower() == 'product':
 					print("Now we have : ")
 					print("ID	 Product	Size 	 Color	 	Quantity")
 					print("001	 T-shirt 	S,M,L    Black	 	10")
 					print("002	 Pants 		S,M,L   red,blue 	10")
-		if i.lower() == 'suggest':
+		elif i.lower() == 'suggest':
 			x = random.randint(1,3)
 			print(item_list[x] + ' is suggested for you.')
 			print("Do you like it ?")
@@ -39,13 +42,14 @@ while endConversation == False:
 						ans_proc2 = ans.split(' ')
 						# for k in ans_proc2 :
 							# if k in item_list:
-		if i.lower() == 'goodbye' :
+		elif i.lower() == 'goodbye' :
 			print('see you later')
 			endConversation = True
-
-				
-
-
+		else :
+			noUnderstandCount += 1
+	print(str(noUnderstandCount) + ',' + str(len(proc)))
+	if noUnderstandCount == len(proc):
+		print ('Shop: Sorry, I cannot understand it')
 
 # findShowTime = "รอบกี่โมง"
 # findShowTimeToken = word_tokenize(findShowTime, engine='newmm')
