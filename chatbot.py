55f222cb4d6834365e31d53923842f4fedb0a10f
volkswg <volkswg@hotmail.com>
2018-05-12 03:45:23 +0700

    almost finish

--- chatbot.py
+++ chatbot.py
@@ -4,10 +4,8 @@ import operator
 
 def main():
 	item_list = ['t-shirt','pants']
-	agree_list = ['yes','sure','of course']
 	hello_list = ['halo','hello','bello','yo','hi']
-	no_list = ['no','not yet','not now','next time','no,']
-	shipping_list = ['ems','kerry','dhl']
+	# shipping_list = ['ems','kerry','dhl']
 	
 	endConversation = False
 
@@ -18,7 +16,7 @@ def main():
 		text = input('Me(Home): ')
 		text = re.sub('[!@#$,]', '', text) # clean sentence (not alphabet)
 		proc = text.split(' ')
-		retCheck = contextCheck(proc, hello_list)
+		retCheck = contextCheck(proc)
 		# print (retCheck)
 		
 		# for i in proc:
@@ -26,70 +24,57 @@ def main():
 		if retCheck == 'greeting':
 			helloIndex = random.randint(0,len(hello_list)-1)
 			print('Shop: ' + hello_list[helloIndex] + ', can I help you?')
-			
+		elif retCheck == 'agree':
+			print('Shop: what?')
+		elif retCheck == 'no':
+			print('Shop: What?')
 		elif retCheck == 'ordering':
 			print("Shop: Okay Good, then you can look at the store and copy the item code and I will send you a detail.")
-			# print("Now we have: ")
-			# print("ID	 Product	Size 	 Color	 	Quantity")
-			# print("001	 T-shirt 	S,M,L    Black	 	10")
-			# print("002	 Pants 		S,M,L   red,blue 	10")
-			# print("Shop:  Order me")
-			# ans2 = input('Me(Order): ')
-			# ans_proc2 = ans2.split(' ')
-			# for k in ans_proc2 :
-				# if k in item_list:
-					# print("You order for " + k)
+		# elif retCheck == 'suggesting':
+			# x = random.randint(0,1)
+			# print("Shop: " + item_list[x] + ' is suggested for you.')
+			# print("Shop: Do you like it ?")
+			
+			# retVal = ''
+			# while retVal != 'agree' and retVal != 'no':
+				# comfirmCheck = input('Me: ')
 					
-					# shipping_check = 1
-					# count = 0
-					# while shipping_check == 1:
-						# print("How can I send you our products?")
-						# ans_shippping = input('Me: ')
-						# shipping_proc = ans_shippping.split(' ')
-						# for l in shipping_proc:
-							# count = count + 1
-							# if l.lower() in shipping_list:
-								# print('I will send you in 2 days. Thanks for coming.')
-								# shipping_check = 0
-							# else :
-								# if count == len(shipping_proc):
-									# print('We only have 3 shipping method. EMS, KERRY and DHL ')														
-					
-		elif retCheck == 'suggesting':
-			x = random.randint(0,1)
-			print("Shop: " + item_list[x] + ' is suggested for you.')
-			print("Shop: Do you like it ?")
-			ans = input('Me: ')
+				# retVal = contextCheck(comfirmCheck.split(' '))
+				# if retVal == 'agree':
+					# print('Shop: Okay, I recorded it, the owner will contract you shortly, thank you :)')
+					# print('Shop: Tell me if you want anything')
+				# if retVal == 'no':
+					# print('Shop: Okay, tell me if you want some thing')
 
-			if ans.lower() in agree_list :
-				print("Shop: Do you like to order now?")
-				ans = input('Me: ')
-				ans_proc = ans.split(' ')
-				for j in ans_proc:
-					if j in agree_list:
-						print("Shop:  Order me")
-						ans2 = input('Me(Order): ')
-						ans_proc2 = ans2.split(' ')
-						for k in ans_proc2 :
-							if k in item_list:
-								print("You order for " + k)
+			# if ans.lower() in agree_list :
+				# print("Shop: Do you like to order now?")
+				# ans = input('Me: ')
+				# ans_proc = ans.split(' ')
+				# for j in ans_proc:
+					# if j in agree_list:
+						# print("Shop:  Order me")
+						# ans2 = input('Me(Order): ')
+						# ans_proc2 = ans2.split(' ')
+						# for k in ans_proc2 :
+							# if k in item_list:
+								# print("You order for " + k)
 								
-								shipping_check = 1
-								count = 0
-								while shipping_check == 1:
-									print("How can I send you our products?")
-									ans_shippping = input('Me: ')
-									shipping_proc = ans_shippping.split(' ')
-									for l in shipping_proc:
-										count = count + 1
-										if l.lower() in shipping_list:
-											print('I will send you in 2 days. Thanks for coming.')
-											shipping_check = 0
-										else :
-											if count == len(shipping_proc):
-												print('We only have 3 shipping method. EMS, KERRY and DHL ')														
-			else :
-				print ("Shop: Okay, I'm sorry")
+								# shipping_check = 1
+								# count = 0
+								# while shipping_check == 1:
+									# print("How can I send you our products?")
+									# ans_shippping = input('Me: ')
+									# shipping_proc = ans_shippping.split(' ')
+									# for l in shipping_proc:
+										# count = count + 1
+										# if l.lower() in shipping_list:
+											# print('I will send you in 2 days. Thanks for coming.')
+											# shipping_check = 0
+										# else :
+											# if count == len(shipping_proc):
+												# print('We only have 3 shipping method. EMS, KERRY and DHL ')														
+			# else :
+				# print ("Shop: Okay, I'm sorry")
 		elif retCheck == 'notUnderstand':
 			print ('Shop: Sorry, I cannot understand it.')
 		elif retCheck == 'ending':
@@ -108,41 +93,52 @@ def cleanWord(sentence):
 	
 def checkItemCodeDetail(itemCode):
 	item_list = [['IC1234','T-shirt','S,M,L','Red'],['IC2341','Pant','S,M,L','Blue'] ]
+	check = -1
 	for i in range(0,len(item_list)):
 		if itemCode == item_list[i][0]:
 			print ('Shop: This item is a' + item_list[i][1] + ' and we have size ' + item_list[i][2] + ' and we have color in ' + item_list[i][3])
-			answer = input('      What do you want (Example: size: m , color: red, quantity: 3)\nMe:')
+			while check == -1:
+				answer = input('      What do you want (Example: size: m , color: red, quantity: 3)\nMe:')
+				check = orderCheck(itemCode,item_list[i][1],answer)
 	# print ('test item.' + itenCode + str(item_list[1]))
 
-def orderCheck(answer):
-	orderDetail = answer.split(',')
-	print(orderDetail)
+def orderCheck(itemcode,products,answer):
+	orderDetail = [x.strip() for x in answer.split(',')]
+	# print(orderDetail)
+	if len(orderDetail) != 3:
+		print('Shop: Invalid Input please input like the example')
+		return -1
+	else:
+		eachDetail = []
+		for each in orderDetail:
+			eachDetail.append(each.split(':'))
+		# print (eachDetail)
+		print('Shop: You odered ' + products + ' with size ' + eachDetail[0][1].upper() + ' color ' + eachDetail[1][1].upper() + ' amount ' + eachDetail[2][1])
+		# comfirmCheck = input('Shop: That right?\nMe:')
+		retVal = ''
+		while retVal != 'agree' and retVal != 'no':
+			if retVal == '':
+				comfirmCheck = input('Shop: That right?\nMe:')
+			else:
+				comfirmCheck = input('Shop: What did you say?\nMe:')
+				
+			retVal = contextCheck(comfirmCheck.split(' '))
+			if retVal == 'agree':
+				print('Shop: Okay, I recorded it, the owner will contract you shortly, thank you :)')
+				print('Shop: Tell me if you want anything')
+			if retVal == 'no':
+				print('Shop: Okay, tell me if you want some thing')
+		return 1
 	
-def contextCheck(wordProc, hello_list):
+def contextCheck(wordProc):
 	# Variable declaration
-
+	agree_list = ['yes','sure','of course']
+	hello_list = ['halo','hello','bello','yo','hi']
+	no_list = ['no','not yet','not now','next time','no,']
+	
 	shipping_list = ['kerry','ems','dhl']
 	ordering_list = ['buy','order','purchase']
 	itemCode_list = ['IC1234','IC1244']
-	
-	# itemCode_list = {
-		# "IC1234": {
-			# "products": "T-shirt",
-			# "size": "S M L",
-			# "color": "Red"
-		# }
-	# }
-	
-	# itemCode_list.update({
-		# "IC1244": {
-			# "products": "T-shirt",
-			# "size": "S M L",
-			# "color": "Red"
-		# }
-	# })
-	
-	# for key,value in itemCode_list.items():
-		# print('Code: ' + str(key) + ',' + str(value))
 	
 	# print(itemCode_list)
 	
@@ -154,6 +150,10 @@ def contextCheck(wordProc, hello_list):
 	for i in wordProc:
 		if i.lower() in hello_list :
 			retProcess.update({1:'greeting'})
+		elif i.lower() in agree_list:
+			retProcess.update({1:'agree'})
+		elif i.lower() in no_list:
+			retProcess.update({1:'no'})	
 		elif i.lower() in ordering_list:
 			retProcess.update({3:'ordering'})
 		elif i.lower() == 'suggest':
@@ -195,30 +195,3 @@ if __name__ == '__main__':
 		sys.exit(0)
 	except SystemExit:
 		os._exit(0)
-
-
-# findShowTime = "รอบกี่โมง"
-# findShowTimeToken = word_tokenize(findShowTime, engine='newmm')
-# findShowTimeCount = 0
-
-# findMovieName = "กับตันอเมริกาไอร่อนแมน"
-# findMovieNameToken = word_tokenize(findMovieName, engine='newmm')
-# findMovieNameCount = 0
-
-
-# for i in range(len(proc)):
-#     for j in range(0, len(findShowTimeToken)):
-#         if LK82(proc[i]) == LK82(findShowTimeToken[j]):
-#             findShowTimeCount = findShowTimeCount+1;
-
-
-# for i in range(len(proc)):
-#     for j in range(0, len(findMovieNameToken)):
-#         if LK82(proc[i]) == LK82(findMovieNameToken[j]):
-#             findMovieNameCount = findMovieNameCount+1
-
-
-# if findShowTimeCount > findMovieNameCount:
-#     print("ShowTime")
-# else:
-#     print("Avenger")
