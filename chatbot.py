6c68ae983bd89cfd531647c5740e1548170ac380
volkswg <volkswg@hotmail.com>
2018-05-11 16:24:32 +0700

    Main Created Again

--- chatbot.py
+++ chatbot.py
@@ -1,60 +1,71 @@
 import random
 
-no_list = ['no','not yet','not now','next time','no,']
-hello_list = ['Hi!','halo','hello','bello','yo']
-item_list = ['t-shirt','pants']
-agree_list = ['yes','sure','of course']
-shipping_list = ['kerry','ems','dhl']
+def main():
+	no_list = ['no','not yet','not now','next time','no,']
+	hello_list = ['Hi!','halo','hello','bello','yo']
+	item_list = ['t-shirt','pants']
+	agree_list = ['yes','sure','of course']
+	shipping_list = ['kerry','ems','dhl']
 
-endConversation = False
-noUnderstandCount = 0
-print("Shop: Hi, I'm a bot. Call me SellingBot.")
-print("Shop: How can I help you.")
-
-while endConversation == False:
+	endConversation = False
 	noUnderstandCount = 0
-	text = input('Me: ') #What products are you have?
-	proc = text.split(' ')
-	for i in proc:
-		if i.lower() in hello_list :
-			helloIndex = random.randint(1,len(hello_list)-1)
-			print('Shop: ' + hello_list[helloIndex] + ', please say what you need.')
-		elif i.lower() == 'buy':
-			for i in proc:
-				if i.lower() == 'something' or i.lower() == 'product':
-					print("Now we have : ")
-					print("ID	 Product	Size 	 Color	 	Quantity")
-					print("001	 T-shirt 	S,M,L    Black	 	10")
-					print("002	 Pants 		S,M,L   red,blue 	10")
-		elif i.lower() == 'suggest':
-			x = random.randint(0,len(item_list)-1)
-			print("Shop: " + item_list[x] + ' is suggested for you.')
-			print("Shop: Do you like it ?")
-			ans = input('Me: ')
+	print("Shop: Hi, I'm a bot. Call me SellingBot.")
+	print("Shop: How can I help you.")
 
-			if ans.lower() in agree_list :
-				print("Shop: Do you like to order now?")
+	while endConversation == False:
+		noUnderstandCount = 0
+		text = input('Me: ') #What products are you have?
+		proc = text.split(' ')
+		for i in proc:
+			if i.lower() in hello_list :
+				helloIndex = random.randint(1,len(hello_list)-1)
+				print('Shop: ' + hello_list[helloIndex] + ', please say what you need.')
+			elif i.lower() == 'buy':
+				for i in proc:
+					if i.lower() == 'something' or i.lower() == 'product':
+						print("Now we have : ")
+						print("ID	 Product	Size 	 Color	 	Quantity")
+						print("001	 T-shirt 	S,M,L    Black	 	10")
+						print("002	 Pants 		S,M,L   red,blue 	10")
+			elif i.lower() == 'suggest':
+				x = random.randint(0,len(item_list)-1)
+				print("Shop: " + item_list[x] + ' is suggested for you.')
+				print("Shop: Do you like it ?")
 				ans = input('Me: ')
-				ans_proc = ans.split(' ')
-				for j in ans_proc:
-					if j in agree_list:
-						print("Shop:  Order me")
-						ans2 = input('Me: ')
-						ans_proc2 = ans.split(' ')
-						for k in ans_proc2 :
-							if k in item_list:
-								print("You order for " + k + ", What shipping method do you prefer?")
-								ans_shippping = input('Me: ')			
+
+				if ans.lower() in agree_list :
+					print("Shop: Do you like to order now?")
+					ans = input('Me: ')
+					ans_proc = ans.split(' ')
+					for j in ans_proc:
+						if j in agree_list:
+							print("Shop:  Order me")
+							ans2 = input('Me: ')
+							ans_proc2 = ans.split(' ')
+							for k in ans_proc2 :
+								if k in item_list:
+									print("You order for " + k + ", What shipping method do you prefer?")
+									ans_shippping = input('Me: ')			
+				else :
+					print ("Shop: Okay, I'm sorry")
+			elif i.lower() == 'goodbye' :
+				print('Shop: see you later')
+				endConversation = True
 			else :
-				print ("Shop: Okay, I'm sorry")
-		elif i.lower() == 'goodbye' :
-			print('Shop: see you later')
-			endConversation = True
-		else :
-			noUnderstandCount += 1
-	# print(str(noUnderstandCount) + ',' + str(len(proc)))
-	if noUnderstandCount == len(proc):
-		print ('Shop: Sorry, I cannot understand it')
+				noUnderstandCount += 1
+		# print(str(noUnderstandCount) + ',' + str(len(proc)))
+		if noUnderstandCount == len(proc):
+			print ('Shop: Sorry, I cannot understand it')
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		print ('Interrupted ..')
+	try:
+		sys.exit(0)
+	except SystemExit:
+		os._exit(0)
 
 # findShowTime = "รอบกี่โมง"
 # findShowTimeToken = word_tokenize(findShowTime, engine='newmm')
